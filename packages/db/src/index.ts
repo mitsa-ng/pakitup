58dbfd927961c2d49ec4cf3d2c22cb1f30bbecea
Nati <yczmzdbrwq@privaterelay.appleid.com>
2026-08-03 20:53:31 +0800

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,10 @@
+import { neon } from "@neondatabase/serverless";
+import { env } from "@pakitup/env/server";
+import { drizzle } from "drizzle-orm/neon-http";
+
+import * as schema from "./schema";
+
+export function createDb() {
+  const sql = neon(env.DATABASE_URL || "");
+  return drizzle(sql, { schema });
+}
